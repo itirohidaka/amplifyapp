568191c517d982bfab6cfabc4e4ae660a648b336
Gerson Itiro Hidaka <itirohidaka@Gersons-MBP.lan>
2020-12-13 16:48:37 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {/* Edited */}
+          <h2>
+            Itiro Edited!
+          </h2>
         </p>
         <a
           className="App-link"
